a613dd74bb976b56f4eafd95ba3594a0708a4c29
Non't <90344104+Shef905015@users.noreply.github.com>
2023-10-25 08:24:14 +0200

    Add files via upload

---
+++ klasaStringZadatak2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringZadatak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1;
+
+            Console.WriteLine("Unesite niz znakova");
+            s1 = Console.ReadLine();
+
+            Console.WriteLine(s1.Replace(" ", "_"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
